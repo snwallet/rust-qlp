963fd401121506b97f0bb67d393c95bb3b2c4e27
qlp <1504001080@qq.com>
2021-01-04 14:56:04 +0800

    模块函数的写法

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,5 +1,21 @@
+
+
+
 fn main() {
-   let x:i32=2;
-   let p:i32=x.pow(3);
-   println!("2^3={}",p);
+ 
+   let x:i32=20;
+   let y:i32=35;
+   let max:i32=CompareValue(x,y);
+  println!("max={}",max);
+}
+
+fn CompareValue(v1:i32,v2:i32)->i32
+{
+    if v1>v2
+    {
+        return v1;
+    }else
+    {
+        return v2;
+    }
 }
